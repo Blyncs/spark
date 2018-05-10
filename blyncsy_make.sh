41cf4cbbc60c11c02a7586fad02f83f32edaa91c
David Lewis <david.lewis@blyncsy.com>
2018-05-09 18:02:47 -0600

    merge

--- blyncsy_make.sh
+++ blyncsy_make.sh
@@ -3,5 +3,5 @@ set -x
 
 export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m"
 
-./build/mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.9.0 -DskipTests clean package
+./build/mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.3 -DskipTests clean package
 
